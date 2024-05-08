0a708cfca2b36be181fd1e38755a1fb580b20477
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-05-08 19:39:55 +0800

    fix: sync with query.sql

--- grouped.sql
+++ grouped.sql
@@ -2,5 +2,6 @@ SELECT applicationId,
     SUM(timeUsed) AS totalUsage -- Unix timestamp
 FROM usageStats
 WHERE hidden = 0
+    AND day > 0 -- epoch day
 GROUP BY applicationId
 ORDER BY totalUsage DESC;
