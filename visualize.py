908cfe055564fb3acc4f1a88ad05ae8615749f97
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-05-08 19:38:33 +0800

    feat: nested treemap

--- visualize.py
+++ visualize.py
@@ -3,12 +3,15 @@
 
 # Database schema
 # https://codeberg.org/fynngodau/usageDirect/src/branch/main/Application/schemas/godau.fynn.usagedirect.persistence.HistoryDatabase/5.json
+# Plotly sunburst
+# https://plotly.com/python/sunburst-charts/
 
 import os
 import pandas as pd
 import sqlite3
 import plotly.graph_objects as go
 import plotly.io as pio
+
 
 # Connect to SQLite DB
 DB = "usageDirect-history.sqlite3"
@@ -43,6 +46,17 @@ def generate_sunburst_chart(df):
     pio.write_html(fig, output_file)
 
 
+def generate_nested_treemap(df):
+    fig = go.Figure(
+        go.Treemap(
+            labels=df["applicationId"], parents=[""] * len(df), values=df["totalUsage"]
+        )
+    )
+
+    output_file = os.path.join(output_dir, "nested-treemap.html")
+    pio.write_html(fig, output_file)
+
+
 # print(sql_query("query.sql", conn))
 df = sql_query("grouped.sql", conn)
 conn.close()
@@ -52,3 +66,4 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 generate_sunburst_chart(df)
+generate_nested_treemap(df)
